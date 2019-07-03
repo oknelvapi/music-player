bb0188391789efe24a24095d2de66cbe47022c14
Ihor P <oknelvapi@yahoo.com>
2019-07-03 11:31:21 +0300

    Updates

---
+++ precache-manifest.7a6c77b4e17978277c0f5ce435239e12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "41ea00361ca4001031e2",
+    "url": "/music_player/static/js/runtime~main.8f706da0.js"
+  },
+  {
+    "revision": "6c64daf529525fec07a1",
+    "url": "/music_player/static/js/main.fa7a9c32.chunk.js"
+  },
+  {
+    "revision": "92c70be0a129cf497808",
+    "url": "/music_player/static/js/2.bf187aa2.chunk.js"
+  },
+  {
+    "revision": "6c64daf529525fec07a1",
+    "url": "/music_player/static/css/main.9f6ac376.chunk.css"
+  },
+  {
+    "revision": "92c70be0a129cf497808",
+    "url": "/music_player/static/css/2.96635e4a.chunk.css"
+  },
+  {
+    "revision": "ac66717cc581358d61928df8b3c02940",
+    "url": "/music_player/index.html"
+  }
+];
